f399ec90cf2878a25eb33e3273f1b67064237110
Bao <teo20191817@gmail.com>
2021-11-12 16:57:35 +0700

    tinh tong

--- source.cpp
+++ source.cpp
@@ -3,6 +3,11 @@ using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 
+int tong(int a, int b)
+{
+	return a + b;
+}
+
 int main()
 {
 	int a, b;
@@ -13,6 +18,11 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
+	if (phepToan == '+')
+	{
+		cout << "Tong la:" << tong(a, b);
+	}
+
 	system("pause");
 	return 0;
 }
